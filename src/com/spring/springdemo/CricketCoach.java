5781de08f62fec92e05f97bea58cb7b5dad93bf7
MOURSELAS Sokratis <smoursel@intrasoft-intl.com>
2019-06-14 11:44:16 +0300

    added Cricket Coach - no implementation

--- src/com/spring/springdemo/CricketCoach.java
+++ src/com/spring/springdemo/CricketCoach.java
@@ -6,14 +6,21 @@ public class CricketCoach implements Coach {
 
     private FortuneService fortuneService;
 
+    // create a no-arg constructor
+
+
+    public CricketCoach() {
+        System.out.println("Cricket Coach: inside the no-arg constructor");
+    }
+
     @Override
     public String getDailyWorkout() {
-        return null;
+        return "Practice fast bowling for 15 minutes";
     }
 
     @Override
     public String getDailyFortune() {
-        return null;
+        return fortuneService.getFortune();
     }
 
     public void setFortuneService(FortuneService fortuneService){
