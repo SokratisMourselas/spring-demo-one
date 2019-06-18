990de0da6df858bf21846e5ac399b1df4fa8a517
MOURSELAS Sokratis <smoursel@intrasoft-intl.com>
2019-06-18 10:13:56 +0300

    added my random fortune service

---
+++ src/com/spring/springdemo/RandomFortuneService.java
@@ -0,0 +1,24 @@
+package com.spring.springdemo;
+
+import com.spring.springdemo.Services.FortuneService;
+
+public class RandomFortuneService implements FortuneService {
+
+
+    private String CLASS_NAME = "RandomFortuneService.class";
+
+    private String[] randomFortuneList = {"Workout no.1","Missed Workout", "15 min. Burn energize"};
+
+    @Override
+    public String getFortune(){
+        return "Today is you lucky day";
+    }
+
+    @Override
+    public String getRandomFortune(){
+
+        return CLASS_NAME + ": " + randomFortuneList[(int)(Math.random() * randomFortuneList.length)];
+
+    }
+
+}
