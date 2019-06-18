a6a1706f645a618f7067854f20fff766f3bdb9e9
MOURSELAS Sokratis <smoursel@intrasoft-intl.com>
2019-06-18 11:21:21 +0300

    bean lifecycle

--- src/com/spring/springdemo/TrackCoach.java
+++ src/com/spring/springdemo/TrackCoach.java
@@ -1,6 +1,25 @@
 package com.spring.springdemo;
 
+import com.spring.springdemo.Services.FortuneService;
+
 public class TrackCoach implements Coach {
+
+	private FortuneService fortuneService;
+
+	public TrackCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
+
+
+	// add an init method
+	public void startup(){
+		System.out.println("TrackCoach: Startup stuff");
+	}
+
+	// add a destroy method
+	public void shutdown(){
+		System.out.println("\nTrackCoach: Shutdown stuff");
+	}
 
 	@Override
 	public String getDailyWorkout() {
---
+++ src/com/spring/springdemo/mainMethods/beanScopeDemo.java
@@ -0,0 +1,31 @@
+package com.spring.springdemo.mainMethods;
+
+import com.spring.springdemo.Coach;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class beanScopeDemo {
+
+
+
+    public static void main(String[] args) {
+
+        //initializing context
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+
+        // loading beans
+        Coach alphaCoach = context.getBean("myCoach", Coach.class);
+        Coach betaCoach = context.getBean("myCoach", Coach.class);
+
+        boolean result = (alphaCoach == betaCoach);
+
+        System.out.println("\nPointing to the same object: " + result);
+
+        System.out.println("\nMemory location of alphaCoach: "+ alphaCoach);
+
+        System.out.println("\nMemory location of alphaCoach: "+ betaCoach);
+
+        //context closed
+        context.close();
+    }
+
+}
